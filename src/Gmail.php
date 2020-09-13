6393fdd7a60dd6a3ca5e5d6d9efeb88b2397487a
Viktor Szépe <viktor@szepe.net>
2020-09-13 08:55:16 +0000

    Implement Gmail

---
+++ src/Gmail.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SzepeViktor\UniqueEmailAddress;
+
+use SzepeViktor\UniqueEmailAddress\Rules\RemoveSeparatorRule;
+use SzepeViktor\UniqueEmailAddress\Rules\RemoveTagRule;
+
+final class Gmail extends EmailProvider implements EmailProviderInterface
+{
+    public function __construct()
+    {
+        $this->domains = ['gmail.com', 'googlemail.com'];
+        $this->addRule(RemoveTagRule::class, ['+']);
+        $this->addRule(RemoveSeparatorRule::class, ['.']);
+    }
+}
